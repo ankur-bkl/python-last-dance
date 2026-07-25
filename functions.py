07e95d9dfe89f1897842b0b329a9efeb0fe8e27e
Ankur Sardar <ankursardar.work@gmail.com>
2026-07-25 16:05:50 +0530

    list functions tuples

---
+++ functions.py
@@ -0,0 +1,13 @@
+def sayHi():
+    print("Hello")
+    
+sayHi()
+
+
+def sayHi2(n):
+    print("Hello " * n)
+    
+
+sayHi2(int(input("Enter the repetition: ")))
+
+    
---
+++ list.py
@@ -0,0 +1,5 @@
+#lists are mutable
+friends =["Kevin", "karen", "Jim", "Oscar", "Toby"]
+print(friends[-1]) #last element index
+print(friends[1:3]) #from 1 to 2 excluding 3
+print(friends) 
