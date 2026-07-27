6bc0e70457e11450e78c0d5351a0671add17503c
Ankur Sardar <ankursardar.work@gmail.com>
2026-07-28 00:55:43 +0530

    feat: new concepts

--- readFile.py
+++ readFile.py
@@ -1,6 +1,9 @@
 
-txtFile =open("file.txt","a+")
-txtFile.write("\nKya Bawasir Banaya Hai")
-txtFile.seek(0)
+# txtFile =open("file.txt","a+")
+# txtFile.write("\nKya Bawasir Banaya Hai")
+# txtFile.seek(0)
+# txtFile.close()  
+# print(txtFile.read())
+txtFile =open("file.txt","r")
 print(txtFile.readlines())
-txtFile.close()  
+txtFile.close()
