3538155e5a54e88242a68f4d43bf5f11ed665678
Ankur Sardar <ankursardar.work@gmail.com>
2026-08-20 21:25:45 +0530

    feat: learn data structures in pandas

---
+++ CWH/Pandas/dataFrame.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# Data Frame - 2D 
+df =pd.DataFrame({"name":["A","B","C"],"marks":[10,2,4]})
+
+print(df)
---
+++ CWH/Pandas/series.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Series - is a labeled array
+s = pd.Series([1,2,3,4,5], index=['a','b','c','d','e'])
+print(s)
